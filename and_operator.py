c6e3b20fb4f83b92ceebbc6fb6a2cccfe9bf4d43
chetan <chetanlale15@gmail.com>
2025-09-29 10:22:08 +0530

    logical operators code files

---
+++ and_operator.py
@@ -0,0 +1,9 @@
+A = 10
+B = 10
+c = -10
+if A > 0 and B > 0 :
+    print("the number are grater than zero")
+if A > 0 and B > 0 and c > 0:
+    print("the number are not grater than zero")
+else:
+    print("the number are not grater than zero")  
---
+++ not_operator.py
@@ -0,0 +1,5 @@
+num=10
+if(num%2==0):
+    print("even num")
+else:
+    print("odd num")
